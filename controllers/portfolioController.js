7f14a087c6f6307c70890d8520e79d03b2c01a06
Rishisha123 <rishisharmaengineer534@gmail.com>
2024-07-22 22:27:07 +0530

    portfolio
    portfolio rishi

---
+++ controllers/portfolioController.js
@@ -0,0 +1,24 @@
+    const nodemailer = require("nodemailer");
+const sendGridTransport = require("nodemailer-brevo-transport");
+
+//transport
+
+
+const sendEmailController = (req,res) => {
+    try{
+        return res.status(200).send({
+            success:true,
+            message:'Your Message Send Successfully',
+        });
+    }catch(error){
+        console.log(error)
+        return res.status(500).send({
+            success:false,
+            message: 'Send Email API Error',
+            error,
+        });
+    }
+};
+
+
+module.exports = {sendEmailController};
